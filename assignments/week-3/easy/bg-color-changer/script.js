cb9bebc60bd45cb4b16cfecb5017273953f136d9
Harshal <85643440+DeHsc0@users.noreply.github.com>
2024-11-12 18:07:02 +0530

    Update script.js

--- assignments/week-3/easy/bg-color-changer/script.js
+++ assignments/week-3/easy/bg-color-changer/script.js
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function() {
     document.body.style.backgroundColor = "blue";
   });
 
-  // Set default color when the "Default" button is clicked
+ 
   document.getElementById("def").addEventListener("click", function() {
     document.body.style.backgroundColor = "#323131";
   });
